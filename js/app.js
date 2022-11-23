674b36dfbd871c35ffa3c9365e78c2e53e5422ea
unknown <a9500484054@yandex.ru>
2022-11-23 23:45:56 +0300

    23.11

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        }
+    },
+  });
